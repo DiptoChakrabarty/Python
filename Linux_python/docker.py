782c0bfea70964d211984a4647660a13e7c409cf
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-05-16 22:41:48 +0530

    docker run working

--- Linux_python/docker.py
+++ Linux_python/docker.py
@@ -1,4 +1,5 @@
 import subprocess as sp
+import os
 n=int(input("Provide no of dockers to run: "))
 	
 #id_docker = input("Enter Name")
@@ -8,11 +9,18 @@ for i in range(n):
 	id_docker = input("Enter Name : ")
 	# print("Enter Name")
 	#sp.getoutput("docker run -itd --name {} centos:latest".format(id_docker))
-	sp.getoutput("docker run -itd --name {} centos:latest".format(id_docker))
+	os.system(" docker run -itd --name {} -v /run/media/root/RHEL-7.5\ Server.x86_64:/dvd -v /root/Desktop/rhel7_5_rpm_extras:/rpm -v /root/Desktop/rhel7_extra_new_rpm:/new_rpm -v /root/Desktop/python_lib:/python_lib  centos:latest".format(id_docker))
+	#os.system("docker exec -d  {} rm  -f /etc/yum.repos.d/*".format(id_docker))
+	#sp.getoutput("docker cp yum_file.repo {}:/etc/yum.repos.d/".format(id_docker))
 
-t= input("Which docker do you want to enter : ")
-sp.getoutput("docker start {} ".format(t))
-sp.getoutput("docker attach {}".format(t))
+
+	sp.getoutput("docker cp file.sh {}:/".format(id_docker))
+	sp.getoutput("docker exec {} bash file.sh".format(id_docker))
+
+	sp.getoutput("docker cp yum_file.repo {}:/etc/yum.repos.d/".format(id_docker))
+#t= input("Which docker do you want to enter : ")
+#sp.getoutput("docker start {} ".format(t))
+#os.system("docker attach {}".format(t))
 
 
 # sp.getoutput("bash docker.sh")
