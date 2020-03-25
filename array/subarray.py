591ed50373cb84f68e5286620268f299b033a094
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2020-03-26 00:41:30 +0530

    Update subarray.py

--- array/subarray.py
+++ array/subarray.py
@@ -1,1 +1,29 @@
 
+#code
+def check(s,l):
+    curr = l[0]
+    start = 0
+    i=1
+    n=len(l)
+    while(i<=n):
+        while curr > s and i-1> start:
+            curr =curr - l[start]
+            start = start+1
+        if curr==s:
+            print (start+1,i)
+            return 1
+        if i<n:
+            curr = curr+l[i]
+        i=i+1
+    print(-1)
+    return 0
+        
+        
+    return 0
+t= int(input())
+for i in range(t):
+    n,s = list(map(int,input().split()))
+    l = list(map(int,input().split()))
+    check(s,l)
+    
+    
