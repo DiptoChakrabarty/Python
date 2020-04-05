7b3639e83c8c9a92f52a9a4ac9a9278137576154
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2020-04-05 19:10:33 +0530

    Create majorityelement.py

---
+++ array/majorityelement.py
@@ -0,0 +1,38 @@
+https://www.geeksforgeeks.org/majority-element/
+def maxelement(l):
+    maxel=0
+    count=1
+    for i in range(len(l)):
+        if l[maxel]==l[i]:
+            count+=1
+        else:
+            count-=1
+        if count==0:
+            maxel=i
+            count=1
+    return l[maxel]
+
+def half(l,n):
+    count=0
+    for i in range(len(l)):
+        if n==l[i]:
+            count+=1
+    if count > n/2:
+        return True
+    else:
+        return False
+
+def showelement(l):
+    n =maxelement(l)
+    if half(l,n):
+        print(n)
+    else:
+        print(-1)
+
+t = int(input())
+for k in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    showelement(l)
+
+                
